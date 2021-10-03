b457db947f3716b0bb4c0f836db055791d96f137
tcharles w. barreto <tcharlesigt@gmail.com>
2021-10-03 15:54:39 -0300

    First commit start project

---
+++ src/main/java/com/tcharles/nfescrapper/NfescrapperApplication.java
@@ -0,0 +1,13 @@
+package com.tcharles.nfescrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfescrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfescrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcharles/nfescrapper/NfescrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcharles.nfescrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfescrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
